d17439ac303ea0d64da73b2fa1d638290121de03
fr0sty-m <fr0sty13@yandex.com>
2026-04-27 16:25:54 +0300

    more special workspaces

--- hypr/scripts/keyboardlayout.sh
+++ hypr/scripts/keyboardlayout.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Hyprctl üzerinden aktif klavye düzenini al
-# jq ile ana klavyeyi seçip ismini çekiyoruz
 LAYOUT=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')
 
-# İsimleri isteğine göre kısaltabilirsin (Opsiyonel)
 case $LAYOUT in
 "Turkish") LAYOUT="TR" ;;
 "English (US)") LAYOUT="EN" ;;
@@ -12,5 +9,4 @@ case $LAYOUT in
 *) LAYOUT=$LAYOUT ;;
 esac
 
-# -r 9911: Mako'da eski bildirimin üzerine yazar, ekran dolmaz.
 notify-send -u low -r 9911 -t 1500 "⌨ Layout" "Current: <b>$LAYOUT</b>"
